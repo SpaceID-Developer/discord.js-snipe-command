c0f6634b2012f3030dd27f0ff47aadb0a5b07ac2
SpaceID-Developer <86755228+SpaceID-Developer@users.noreply.github.com>
2021-07-01 11:41:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const snipes = new Discord.Collection();
-const token = "YOUR_TOKEN_HERE";
+const token = "YOUR_TOKEN_HERE";//input your token here
 
 client.on("messageDelete", message => {
   snipes.set(message.channel.id, message)
 })
 
 client.on("message", message => {
-  if (message.content === "-status") {
+  if (message.content === "-status") { //test online bot
     return message.channel.send("My Status is **Online!**");
   }
   
-  if (message.content.startsWith("-snipe")) {
+  if (message.content.startsWith("-snipe")) { //change prefix in here
     let snipe = snipes.get(message.channel.id)
     if (!snipe) return message.channel.send("No previously deleted chat!!")
     
